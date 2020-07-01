2e7302711d538c706fdd6d31865f17cc215adc1a
Daniel Ben-Lulu <d.benlulu25@gmail.com>
2020-07-01 19:30:24 +0200

    Remove useless NullTrackProvider

--- index.ts
+++ index.ts
@@ -1,23 +1,18 @@
 
 import ITrackProvider, { OnTrackAddedCallback, StringField } from '@providers/ITrackProvider'
 
-import NullTrackProvider from '@providers/NullTrackProvider'
 import LocalTrackProvider from '@providers/LocalTrackProvider'
 
-const NULL_TRACK_PROVIDER = 'NullTrackProvider'
 const LOCAL_TRACK_PROVIDER = 'LocalTrackProvider'
 
 class TrackProviderFactory {
 
   getAvailableTrackProviders(): Array<string> {
-    return [ NULL_TRACK_PROVIDER, LOCAL_TRACK_PROVIDER ]
+    return [ LOCAL_TRACK_PROVIDER ]
   }
 
   getTrackProvider(providerName: string, filesDir: string, callback: OnTrackAddedCallback): ITrackProvider | null {
     switch (providerName) {
-      case NULL_TRACK_PROVIDER: {
-        return new NullTrackProvider()
-      }
       case LOCAL_TRACK_PROVIDER: {
         return new LocalTrackProvider(filesDir, callback)
       }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -7,10 +7,10 @@ import { TrackProviderFactory, LOCAL_TRACK_PROVIDER } from '@index'
 it('should get every providers', () => {
   const factory = new TrackProviderFactory()
   const providers = factory.getAvailableTrackProviders()
-  expect(providers.length).toEqual(2)
+  expect(providers.length).toEqual(1)
 
-  expect(providers[1]).toEqual(LOCAL_TRACK_PROVIDER)
+  expect(providers[0]).toEqual(LOCAL_TRACK_PROVIDER)
 
-  expect(factory.getTrackProvider(providers[1], '', () => { return })).toBeInstanceOf(LocalTrackProvider)
+  expect(factory.getTrackProvider(providers[0], '', () => { return })).toBeInstanceOf(LocalTrackProvider)
 })
 
